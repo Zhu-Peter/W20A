3461734fa5c09a89e54ac3384eb955303cefac32
Zhu-Peter <gzhu@ualberta.ca>
2024-07-10 16:08:14 -0600

    updated database queries

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
 -- Create the following tables: 
 -- client:
--- id (PK)
--- username (unique)
--- joined_on
--- password
+    -- id (PK)
+    -- username (unique)
+    -- joined_on
+    -- password
 
 -- post:
--- id(PK)
--- client_id (FK)
--- content
--- title
+    -- id(PK)
+    -- client_id (FK)
+    -- content
+    -- title
 
 Create Table client (
     id int(11) NOT NULL AUTO_INCREMENT,
@@ -27,4 +27,34 @@ create table post (
     PRIMARY KEY (id)
     FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE
 );
+
+-- INSERT at least 3 clients and 5 posts
+
+insert into client (username, joined_on, password) values 
+    ('john', '2019-01-01', '1234password')
+    ('jane', '2019-02-02', '2345password')
+    ('jill', '2019-03-03', '9876password');
+
+insert into post (client_id, content, title) values 
+    (1, 'This is a post', 'This is a title'),
+    (2, 'This is another post', 'This is another title'),
+    (3, 'This is a third post', 'This is a third title'),
+    (4, 'This is a 4th post', 'This is a fourth title'),
+    (5, 'This is fifth post', 'This is 5th title');
+
+-- Create the following DB procedures:
+
+-- Given a username and password as input, SELECT the id and username  for the user that matches that given input
+
+create procedure login(username varchar(20), password varchar(20))
+    select id, username from client where username = username and password = password;
+
+-- Given a user id, blog content and title as input, INSERT a new row into the post table
+
+create procedure create_post(client_id int, content varchar(200), title varchar(20))
+    insert into post (client_id, content, title) values (client_id, content, title);
+
+-- Given no input, return the id, content and title of each post
+create procedure get_posts()
+    select id, content, title from post;
 
