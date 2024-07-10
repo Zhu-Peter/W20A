d21ebbd71590b12e2a18774cb3e29d6acd312801
Zhu-Peter <gzhu@ualberta.ca>
2024-07-09 19:35:41 -0600

    initial commit

--- queries.sql
+++ queries.sql
@@ -1,10 +1,30 @@
--- client
+-- Create the following tables: 
+-- client:
 -- id (PK)
 -- username (unique)
 -- joined_on
 -- password
--- post
+
+-- post:
 -- id(PK)
 -- client_id (FK)
 -- content
 -- title
+
+Create Table client (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    joined_on datetime NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+create table post (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    client_id int(11) NOT NULL,
+    content varchar(255) NOT NULL,
+    title varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE
+);
+
